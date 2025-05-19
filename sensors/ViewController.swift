40fc4c9a4fae15472d8119910f7f719dd4bcf630
Filip Kirsek <fkirsek@spotify.com>
2025-05-19 19:57:32 +0200

    Added labels, some more refactoring

--- sensors/ViewController.swift
+++ sensors/ViewController.swift
@@ -19,9 +19,29 @@ class ViewController: UIViewController {
     let disposeBag = DisposeBag()
     let viewModel = ViewModel()
 
+    let circleAppearedLabel = UILabel()
+    var circleAppearedLabelXCoord = NSLayoutConstraint()
+    var circleAppearedTimestamp = TimeInterval()
+
+    let circleDisappearedLabel = UILabel()
+    var circleDisappearedLabelXCoord = NSLayoutConstraint()
+
+    let squareAppearedLabel = UILabel()
+    var squareAppearedLabelXCoord = NSLayoutConstraint()
+    var squareAppearedTimestamp = TimeInterval()
+
+    let squareDisappearedLabel = UILabel()
+    var squareDisappearedLabelXCoord = NSLayoutConstraint()
+
+    let circleColor = UIColor.red
+    let squareColor = UIColor.blue
+
     let graphLayer = CAShapeLayer()
     var points: [CGPoint] = []
-    let timeIntervalXModifier = 50.0
+    let timeIntervalXModifier = 100.0
+    var maxCount = 0
+    var width: CGFloat = 0
+    var height: CGFloat = 0
 
     let appStart = Date()
 
@@ -30,39 +50,73 @@ class ViewController: UIViewController {
         setupBindings()
     }
 
+
     private func setupBasicViews() {
         view = UIView()
         view.backgroundColor = .white
 
         view.addSubview(circle)
         view.addSubview(square)
+        view.layer.addSublayer(graphLayer)
 
         square.autoCenterInSuperview()
-        square.backgroundColor = .blue
+        square.backgroundColor = squareColor
         square.autoSetDimensions(to: CGSize(width: size, height: size))
         square.isHidden = true
 
         circle.autoCenterInSuperview()
         circle.autoSetDimensions(to: CGSize(width: size, height: size))
-        circle.backgroundColor = .red
+        circle.backgroundColor = circleColor
         circle.layer.cornerRadius = size / 2
         circle.isHidden = true
     }
 
-    private func drawGraph() {
-        let width = view.frame.width
-        let height = view.frame.height
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        width = view.frame.width
+        height = view.frame.height
+        maxCount = Int(width * 0.5 / (viewModel.updateInterval * timeIntervalXModifier) )
 
+        circleAppearedLabel.text = "Circle appeared"
+        setupLabel(label: circleAppearedLabel, color: circleColor, constraint: &circleAppearedLabelXCoord)
+
+        circleDisappearedLabel.text = "Circle disappeared"
+        setupLabel(label: circleDisappearedLabel, color: circleColor, constraint: &circleDisappearedLabelXCoord)
+
+        squareAppearedLabel.text = "Square appeared"
+        setupLabel(label: squareAppearedLabel, color: squareColor, constraint: &squareAppearedLabelXCoord)
+
+        squareDisappearedLabel.text = "Square disappeared"
+        setupLabel(label: squareDisappearedLabel, color: squareColor, constraint: &squareDisappearedLabelXCoord)
+    }
+
+    private func setupLabel(label: UILabel, color: UIColor, constraint: inout NSLayoutConstraint) {
+        label.textColor = color
+        label.isHidden = true
+        view.addSubview(label)
+        label.autoPinEdge(toSuperviewEdge: .top, withInset: height * 0.25)
+        constraint = label.autoPinEdge(.left, to: .left, of: view, withOffset: 0)
+        label.font = .systemFont(ofSize: 8)
+
+        let lineView = UIView()
+        label.addSubview(lineView)
+        lineView.autoPinEdge(.bottom, to: .top, of: label)
+        lineView.autoPinEdge(.top, to: .top, of: view)
+        lineView.autoSetDimension(.width, toSize: 1)
+        lineView.backgroundColor = color
+    }
+
+    private func drawGraph() {
         let path = UIBezierPath()
         path.move(to: CGPointZero)
-        let maxCount = Int(width * 0.5 / (viewModel.updateInterval * timeIntervalXModifier) )
 
         //TODO: Clunky, move to ViewModel?
-        let graphPointsSlice = points.suffix(maxCount)
-        let startInterval = graphPointsSlice.first?.x ?? 0
-        let graphPoints = graphPointsSlice.map { CGPoint(x: $0.x - startInterval, y: $0.y)  }
+        let startInterval = points.first?.x ?? 0
+        let graphPoints = points.map { CGPoint(x: $0.x - startInterval, y: $0.y)  }
 
-        graphPoints.forEach { path.addLine(to: $0) }
+        graphPoints.forEach { graphPoint in
+            path.addLine(to: graphPoint)
+        }
         path.move(to: CGPointZero)
         path.close()
 
@@ -73,29 +127,51 @@ class ViewController: UIViewController {
         graphLayer.strokeColor = UIColor.orange.cgColor
         graphLayer.fillColor = UIColor.clear.cgColor
         graphLayer.masksToBounds = true
-        
-        view.layer.addSublayer(graphLayer)
+
+        // Move labels
+        circleAppearedLabelXCoord.constant = CGFloat(circleAppearedTimestamp) * timeIntervalXModifier - startInterval
+        squareAppearedLabelXCoord.constant = CGFloat(squareAppearedTimestamp) * timeIntervalXModifier - startInterval
     }
 
     private func setupBindings() {
         viewModel.showSquareTimer
+            .do(onNext: { [weak self] isHidden in
+                guard let self = self else { return }
+                if isHidden {
+                    //squareDisappearedLabel.isHidden = false
+                } else {
+                    squareAppearedLabel.isHidden = false
+                    squareAppearedTimestamp = Date().timeIntervalSince(appStart) //TODO: better to use the timer counter
+                }
+            })
             .bind(to: square.rx.isHidden)
             .disposed(by: disposeBag)
 
         viewModel.showCircleTimer
+            .do(onNext: { [weak self] isHidden in
+                guard let self = self else { return }
+                if isHidden {
+                    //circleDisappearedLabel.isHidden = false
+                } else {
+                    circleAppearedLabel.isHidden = false
+                    circleAppearedTimestamp = Date().timeIntervalSince(appStart) //TODO: better to use the timer counter
+                }
+            })
             .bind(to: circle.rx.isHidden)
             .disposed(by: disposeBag)
 
         viewModel.currentFaceTransform.subscribe(onNext: { _ in  }).disposed(by: disposeBag)
 
-        let height = view.frame.height
-
         viewModel.accelerationObservable
             .subscribe(onNext: { [weak self] acceleration in
                 guard let self = self else { return }
                 let l = (acceleration.x.magnitudeSquared + acceleration.y.magnitudeSquared + acceleration.z.magnitudeSquared).squareRoot()
-                let currentTimestamp = Date().timeIntervalSince(appStart)
-                points.append(CGPoint(x: CGFloat(currentTimestamp) * timeIntervalXModifier , y: l * 100 + height * 0.75))
+                let currentTimestamp = Date().timeIntervalSince(appStart) //TODO: better to use the timer counter
+
+                if points.count > self.maxCount {
+                    points.removeFirst()
+                }
+                points.append(CGPoint(x: CGFloat(currentTimestamp) * timeIntervalXModifier , y: l * 100))
                 self.drawGraph()
         }).disposed(by: disposeBag)
     }
